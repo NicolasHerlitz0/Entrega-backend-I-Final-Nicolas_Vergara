527c7ccb905cc30c9ec5f58d8d4606ddc48e3252
NicolasHerlitz0 <verher22@gmail.com>
2026-01-15 12:11:25 -0300

    gitignoreOjalaFinal

--- src/controllers/products.controller.js
+++
@@ -1,6 +0,0 @@
-// NUEVO (opcional, para mantener routers delgados)
-/*
-(opcional pero recomendado)
-En este script irá la orquestación HTTP de productos (lee query, llama service, usa ok/err).
-Y funcionará para mantener routes delgadas y coherentes.
-*/
